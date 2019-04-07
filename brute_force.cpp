22244e807b1b6c74e8d139315fc4f67bc360350e
wlee221 <wlee221@live.unc.edu>
2019-04-07 17:10:51 -0400

    Added skeleton code for brute force intersection test.

---
+++ brute_force.cpp
@@ -0,0 +1,30 @@
+/*
+ * This program vertifies red blue segment test files using brute-force algorithm. 
+ *
+ * ! @author William Lee
+ */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool vertify_test_file(string);
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) 
+        cerr << "Usage: " << argv[0] << " <file1 file2 ...>" << endl;
+    for (int i = 1; i < argc; ++i) {
+        vertify_test_file(argv[i]);
+    }
+}
+
+bool vertify_test_file(string file_name) {
+    ifstream input(file_name); 
+    if (input.is_open()){
+        int m, n, k;
+        cout << "Testing " << file_name << "..." << endl;
+    } else {
+        cerr << endl << "ERROR: file " << file_name << " cannot be opened." << endl;
+    }
+    return true;
+}
