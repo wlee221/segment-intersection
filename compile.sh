4fa76db6d05eff52d828fc836038e1439c4f4d39
wlee221 <wlee221@live.unc.edu>
2019-04-15 03:11:08 -0400

    Added bash script that compiles cpp files to appropriate executables

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+file_to_executable=( \
+	"brute_force.cpp	BFwilliam" \
+	"sort_flags.cpp		SFwilliam" \
+	"verify_red.cpp		DSwilliam"
+)
+
+for str in "${file_to_executable[@]}"; do 
+	str_arr=($str) # convert string to array to separate white space
+	file=${str_arr[0]}
+	executable=${str_arr[1]}
+	g++ $file -std=c++17 -o $executable
+done
